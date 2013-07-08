6cb75bf594e99a2a3b0026fa75acf395dce793ca
Ubuntu <ubuntu@ip-10-164-37-60.ec2.internal>
2013-07-08 03:19:42 +0000

    readFileSync

--- web.js
+++ web.js
@@ -5,6 +5,7 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
   var data = fs.readFileSync("index.html");
   response.send(data.toString());
+});
   var port = process.env.PORT || 5000;
   app.listen(port, function() {
   console.log("Listening on " + port);
